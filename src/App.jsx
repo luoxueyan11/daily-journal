4da4b7a95dc9d0b781d247bfa0286b786ee80d22
Luo Xueyan <luoxueyan.lxy@gmail.com>
2022-10-13 23:57:00 +0800

    add code for plan display, part of the journal editor display and automatic journal generation

--- src/App.jsx
+++ src/App.jsx
@@ -117,7 +117,7 @@ class TimeLine extends React.Component {
     return (
       <div className="register-container">
         <form name="signup" className="register">
-          <h2>TimeLine!</h2>
+          <h2>TimeLine</h2>
         </form>
       </div>
     );
@@ -125,19 +125,133 @@ class TimeLine extends React.Component {
 }
 
 
+const initialPlan = [
+  {
+    id : 1,
+    startTime : "10:00 am",
+    endTime : "11:00 am",
+    description: "This is task 1",
+    checked: false
+  },
+  {
+    id : 2,
+    startTime : "14:00 pm",
+    endTime : "16:00 pm",
+    description: "This is task 2",
+    checked: false
+  },
+  {
+    id : 3,
+    startTime : "20:00 pm",
+    endTime : "21:00 pm",
+    description: "This is task 3",
+    checked: false
+  }
+]
+
+
+
+class PlanRows extends React.Component {
+  constructor() {
+    super(); 
+  }
+
+  handleChange(e, id,checked) {
+
+    if (!checked){
+      this.props.completePlans(id);
+    } else {
+      this.props.uncheckPlans(id);
+    }
+  }
+
+  render() {
+    const plans = this.props.plans;
+    return (
+      plans.map(plan => 
+        <tr>
+          <td>{plan.id.toString()}</td>
+          <td>{plan.startTime.toString()}</td>
+          <td>{plan.endTime.toString()}</td>
+          <td>{plan.description.toString()}</td>
+          <td><input 
+            type="checkbox" 
+            checked={plan.checked}
+            value={plan.id}
+            onChange={e => this.handleChange(e, plan.id, plan.checked)}
+            /></td>
+        </tr>
+      )
+    )
+  }
+}
+
 class Plan extends React.Component {
   constructor() {
-    super();
+    super(); 
+    this.completePlans = this.completePlans.bind(this);
+    this.uncheckPlans = this.uncheckPlans.bind(this);
+  }
+
+  completePlans(id){
+    this.props.completePlans(id);
+  }
+
+  uncheckPlans(id){
+    this.props.uncheckPlans(id);
   }
 
   render() {
     return (
       <div className="register-container">
         <form name="signup" className="register">
-          <h2>Plan!</h2>
+          <h2>Plan</h2>
+            <table className="bordered-table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Start Time</th>
+                <th>End Time</th>
+                <th>Description</th>
+                <th>Completion Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <PlanRows plans={this.props.plans} completePlans={this.completePlans} uncheckPlans={this.uncheckPlans}/>
+            </tbody>
+            </table>
         </form>
       </div>
     );
+  }
+}
+
+class JournalRows extends React.Component {
+  constructor() {
+    super(); 
+  }
+
+
+  render() {
+    const completed = this.props.completed;
+    return (
+      completed.map(p => 
+        <tr>
+          <td>{p.startTime.toString()}</td>
+          <td>{p.endTime.toString()}</td>
+          <td>{p.description.toString()}</td>
+          <td><input 
+            type="button" 
+            value="Edit"
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href='#editor';
+              this.props.switchPage(2);
+              }}
+            /></td>
+        </tr>
+      )
+    )
   }
 }
 
@@ -145,15 +259,60 @@ class Plan extends React.Component {
 class Journal extends React.Component {
   constructor() {
     super();
+    this.state = { selector: 1}; 
+    this.switchPage = this.switchPage.bind(this);
+  }
+  setSelector(value){
+    this.setState({selector:value});
+    console.log(value);
+  }
+
+  switchPage(value){
+    this.setSelector(value);
+  }
+
+  renderSwitch(param) {
+    switch(param) {
+      case 1:
+        return (
+          <div className="register-container">
+          <form name="signup" className="register">
+            <h2>Journal</h2>
+            <table className="bordered-table">
+              <thead>
+                <tr>
+                  <th>Start Time</th>
+                  <th>End Time</th>
+                  <th>Content</th>
+                  <th>Edit</th>
+                </tr>
+              </thead>
+              <tbody>
+               <JournalRows completed={this.props.completed} switchPage={this.switchPage}/>
+              </tbody>
+              </table>
+          </form>
+        </div>
+        );
+      case 2:
+        return (
+          <div>
+            <div>
+              Editor
+            </div>
+            <div><button onClick={(e) => {this.switchPage(1);}}>back</button></div>
+          </div>
+
+        );
+    }
   }
 
   render() {
     return (
-      <div className="register-container">
-        <form name="signup" className="register">
-          <h2>Journal!</h2>
-        </form>
+      <div>
+        {this.renderSwitch(this.state.selector)}
       </div>
+      
     );
   }
 }
@@ -169,10 +328,6 @@ class InitialPage extends React.Component {
   setSelector(value){
     this.setState({selector:value});
     console.log(value);
-  }
-  
-  componentDidMount() {
-    this.setState({ users: [] });
   }
 
   signUpUser(user){
@@ -204,11 +359,59 @@ class InitialPage extends React.Component {
 class MainPage extends React.Component {
 	constructor() {
 	super();
-  this.state = { selector: 1}; 
+  this.state = {plans:initialPlan, completed:[], selector: 1}; 
+  this.completePlans = this.completePlans.bind(this);
+  this.uncheckPlans = this.uncheckPlans.bind(this);
 	}
 
   setSelector(value){
     this.setState({selector:value});
+  }
+
+  completePlans(id){
+    const plans = this.state.plans;
+    const temp = this.state.completed;
+    for (var i=0; i<plans.length; i++){
+      if (plans[i].id == id){
+        let newComplete = {
+          id: plans[i].id,
+          startTime: plans[i].startTime,
+          endTime: plans[i].endTime,
+          description: plans[i].description,
+          checked: true
+        }
+        plans[i] = newComplete;
+        temp.push(newComplete);
+      }
+    }
+    this.setState({completed: temp.sort((a, b) => (a.id > b.id) ? 1 : -1)}, function () {
+      console.log("check a plan");
+  });
+  }
+
+  uncheckPlans(id){
+    const plans = this.state.plans;
+    const temp = this.state.completed;
+    const updateComplete = temp.filter(function(plan){
+      return plan.id != id;
+    })
+    this.setState({completed: updateComplete}, function () {
+      console.log("uncheck a plan");
+  });
+
+    
+    for (var i=0; i<plans.length; i++){
+      if (plans[i].id == id){
+        let newUncheck = {
+          id: plans[i].id,
+          startTime: plans[i].startTime,
+          endTime: plans[i].endTime,
+          description: plans[i].description,
+          checked: false
+        }
+        plans[i] = newUncheck;
+      }
+    }
   }
 
 	render(){
@@ -226,17 +429,14 @@ class MainPage extends React.Component {
     <form className="register">
     <div>
       {this.state.selector == 1 && (<TimeLine/>)}
-      {this.state.selector == 2 && (<Plan/>)}
-      {this.state.selector == 3 && (<Journal/>)}
+      {this.state.selector == 2 && (<Plan plans={this.state.plans} completed={this.state.completed} completePlans={this.completePlans} uncheckPlans={this.uncheckPlans}/>)}
+      {this.state.selector == 3 && (<Journal completed={this.state.completed}/>)}
   </div>
   </form>
 </div>
   );
 	}
 }
-
-
-
 
 
 class WebPage extends React.Component{
@@ -249,6 +449,10 @@ class WebPage extends React.Component{
 
   setSelector(value){
     this.setState({selector:value});
+  }
+
+  componentDidMount() {
+    this.setState({ users: [] });
   }
 
   switchPage(selector){
