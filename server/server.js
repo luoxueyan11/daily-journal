b79ae6e605f5618ebe595e4d8861eeb3e9b3ed6b
Luo Xueyan <luoxueyan.lxy@gmail.com>
2022-11-20 15:41:51 +0800

    update server.js, add schema.graphql

--- server/server.js
+++ server/server.js
@@ -1,9 +1,60 @@
+const fs = require('fs');
 const express = require('express');
+const { ApolloServer, UserInputError } = require('apollo-server-express');
+const { GraphQLScalarType } = require('graphql');
+const { Kind } = require('graphql/language');
+const { MongoClient } = require('mongodb');
+
+let db;//Variable that points to the real DB.
+
+
+
+async function listUsers()
+{
+}
+
+
+const resolvers = {
+  Query: {
+    listUsers
+  },
+
+};
+
+
 
 const app = express();
-
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('App started on port 3000');
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
+  resolvers,
+  formatError: error => {
+    console.log(error);
+    return error;
+  },
 });
+async function applyGraphql() {
+  await server.start();
+  server.applyMiddleware({ app, path: '/graphql' });
+}
+
+
+async function connectToDb() {
+	  const url = 'mongodb://localhost/project';
+	  const client = new MongoClient(url, { useNewUrlParser: true });
+	  await client.connect();
+	  console.log('Connected to project MongoDB at', url);
+	  db = client.db();
+}
+
+(async function () {
+  try {
+    await connectToDb();
+    app.listen(3000, function () {
+      console.log('App started on port 3000');
+    });
+  } catch (err) {
+    console.log('ERROR:', err);
+  }
+})();
